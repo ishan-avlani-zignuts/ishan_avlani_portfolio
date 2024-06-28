d041c99e69ddcebad8318eea88172d4291ea8dd6
Ishan Avlani <ishana@zignuts.com>
2024-06-28 16:43:08 +0530

    change of ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -73,10 +73,6 @@ const Navbar = () => {
             WebkitTextFillColor: "transparent",
             backgroundClip: "text",
             textFillColor: "transparent",
-            "&:hover": {
-              background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-              color: "white",
-            },
           }}
         >
           {"<Ishan Sandip Avlani />"}
